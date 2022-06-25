56f8da712d2edb69755ca5dddfbcfe827ad2a6f7
Yogesh Kumar <yogesh.kumar@trilogy.com>
2022-06-25 08:18:44 +0000

    try

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,13 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as http from 'http';
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
   useEffect(() => {
-    fetch('https://5000-aquamarine-leopon-mmjbfavb.ws.trilogy.devspaces.com/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
+    
+    // axios.get('https://api1:5000/time').then(res => {
+    //   // console.log(res);
+    //   setCurrentTime(res.data.time);
+    // })
+    // fetch('/time').then(res => res.json()).then(data => {
+    //   setCurrentTime(data.time);
+    // });
+    const options = {
+      hostname: 'api1',
+      port: 5000,
+      path: '/time',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(postData)
+      }
+    };
+    const req = http.request(options, (res) => {
+      console.log(`STATUS: ${res.statusCode}`);
+      console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+        console.log(`BODY: ${chunk}`);
+      });
+      res.on('end', () => {
+        console.log('No more data in response.');
+      });
     });
+    
+    req.on('error', (e) => {
+      console.error(`problem with request: ${e.message}`);
+    });
+    // fetch('https://https://5000-indigo-albatross-68skv17a.ws.trilogy.devspaces.com/time').then(res => res.json()).then(data => {
+    //   console.log('Printing data:', data)
+    //   setCurrentTime(data.time);
+    // });
   });
 
   return (
